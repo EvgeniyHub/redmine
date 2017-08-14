447ba26c8d202e911c7825e87e8c6462cf5f7292
EvgeniyHub <30509963+EvgeniyHub@users.noreply.github.com>
2017-08-14 17:55:02 +0300

    Add files via upload

---
+++ storage/framework/views/c5efcaaf6fdf67884e6891b72110d74cd9fc6096.php
@@ -0,0 +1,29 @@
+<?php $__env->startSection('content'); ?>
+<script>
+   function agreeForm(f) {
+    // Если поставлен флажок, снимаем блокирование кнопки
+    if (f.agree.checked) f.emailProfile.disabled = 0 
+    // В противном случае вновь блокируем кнопку
+    else f.emailProfile.disabled = 1
+   }
+</script>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div class="col-md-6"><span > Email</span> <input id="emailProfile" type="email" name="emailProfile"  disabled="false" onclick="agreeForm(this.form)"> <button type="submit" > Edit</button>
+          </div>
+          </div>
+          <div class="col-md-8 col-md-offset-2">
+              <div class="col-md-6"><span > Phone</span> <input id="Phone" type="Phone"  name="Phone" disabled="false" > <button> Edit</button>
+              </div>
+              </div>
+
+              <div class="col-md-8 col-md-offset-2">
+                  <div class="col-md-6"><span > Skype</span> <input id="Phone" type="Skype"  name="Skype" disabled="false" ><button onclick=""> Edit</button>
+                  </div>
+              </div>
+
+      </div>
+      <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
